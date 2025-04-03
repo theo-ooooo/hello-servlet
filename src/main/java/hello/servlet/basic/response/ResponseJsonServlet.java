42c2d8fab110458a22b3108c10afd282d5ef5995
Theo <kkw.theo@gmail.com>
2025-04-03 12:21:34 +0900

    feat: HTTP 응답 데이터 - API JSON

---
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,36 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ *
+ * http://localhost:8080/response-json
+ */
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        HelloData data = new HelloData();
+        data.setUsername("kang");
+        data.setAge(30);
+
+        String result = objectMapper.writeValueAsString(data);
+
+        response.getWriter().write(result);
+
+    }
+}
