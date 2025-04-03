d1a323f87001b11dcdda766e3ee53d08e039776c
Theo <kkw.theo@gmail.com>
2025-04-03 13:48:39 +0900

    feat: 서블릿으로 회원 관리 웹 애플리케이션 만들기

--- src/main/java/hello/servlet/domain/Member.java
+++ src/main/java/hello/servlet/domain/Member.java
@@ -8,11 +8,13 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class Member {
     private Long id;
     private String username;
     private int age;
+
+    public Member() {
+    }
 
     public Member(String username, int age) {
         this.username = username;
