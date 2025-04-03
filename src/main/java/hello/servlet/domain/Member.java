d92c035e7d14659a4db6916a27d55aacebd582b2
Theo <kkw.theo@gmail.com>
2025-04-03 12:49:40 +0900

    feat: 회원 관리 웹 애플리케이션 요구사항

---
+++ src/main/java/hello/servlet/domain/Member.java
@@ -0,0 +1,21 @@
+package hello.servlet.domain;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Member {
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
